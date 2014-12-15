2f06e455866a2795b8144432d7d0c2e8ab6ba65a
Flipper63 <flipboer63@gmail.com>
2014-12-15 12:31:49 +0100

    Initial commit SonataSammie

---
+++ app/cache/dev/twig/8e/04/4b361337aa3cb59ea612ae9ef2b30a209ead4113a3521278d9707c1e053c.php
@@ -0,0 +1,49 @@
+<?php
+
+/* SonataBlockBundle:Block:block_core_text.html.twig */
+class __TwigTemplate_8e044b361337aa3cb59ea612ae9ef2b30a209ead4113a3521278d9707c1e053c extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->blocks = array(
+            'block' => array($this, 'block_block'),
+        );
+    }
+
+    protected function doGetParent(array $context)
+    {
+        return $this->env->resolveTemplate($this->getAttribute($this->getAttribute((isset($context["sonata_block"]) ? $context["sonata_block"] : $this->getContext($context, "sonata_block")), "templates", array()), "block_base", array()));
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $this->getParent($context)->display($context, array_merge($this->blocks, $blocks));
+    }
+
+    // line 14
+    public function block_block($context, array $blocks = array())
+    {
+        // line 15
+        echo "    ";
+        echo $this->getAttribute((isset($context["settings"]) ? $context["settings"] : $this->getContext($context, "settings")), "content", array());
+        echo "
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "SonataBlockBundle:Block:block_core_text.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  29 => 15,  26 => 14,);
+    }
+}
